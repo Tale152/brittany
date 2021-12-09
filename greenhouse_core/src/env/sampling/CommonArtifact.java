b54e42c1b290f5d2d68535e6911cdad9d85da8e4
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-12-09 18:31:17 +0100

    Added main structure of JaCaMo to build the greenhouse core

---
+++ greenhouse_core/src/env/sampling/CommonArtifact.java
@@ -0,0 +1,19 @@
+// CArtAgO artifact code for project greenhouse_core
+
+package sampling;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cartago.*;
+
+public class CommonArtifact extends Artifact {
+	
+	private List<String> devices;
+	
+	void init() {
+		devices = new ArrayList<>(Arrays.asList("id1", "id2", "id3"));
+		defineObsProperty("devices", devices);
+	}
+}
---
+++ greenhouse_core/src/env/sampling/SamplingArtifact.java
@@ -0,0 +1,63 @@
+// CArtAgO artifact code for project greenhouse_core
+
+package sampling;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+
+import cartago.*;
+
+public class SamplingArtifact extends Artifact {
+	
+	private List<String> devices;
+	private Optional<Map<String, Integer>> lastSamples;
+	private Map<String, Integer> currentSamples;
+	private Map<String, Integer> deltaDevices;
+	private Map<String, Pair<Integer, Integer>> thresholds;
+	
+	void init() {
+		this.lastSamples = Optional.empty();		
+		this.currentSamples = new HashMap<>();
+		this.deltaDevices = new HashMap<>();
+		this.thresholds = new HashMap<>();
+	}
+	
+	@OPERATION void initDevices(List<String> devices) {
+		this.devices = devices;
+		System.out.println("Init " + devices);
+		
+		devices.forEach(d -> this.currentSamples.put(d, new Random().nextInt()));		
+		devices.forEach(d -> this.deltaDevices.put(d, 200000));
+		devices.forEach(d -> this.thresholds.put(d, new Pair<>(100000,500000)));
+	}
+	
+	@OPERATION void sampleOperation() {
+		//simulating time to obtain samples
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}		
+		this.devices.forEach(d -> this.currentSamples.put(d, new Random().nextInt()));
+		
+		if(this.lastSamples.isEmpty()) {		
+			//TODO mandare dati su db o reperire l'ultima entry su db e controllarla?
+		} else {
+			System.out.println("Last samples " + this.lastSamples + "\n Current samples " + this.currentSamples);
+			this.currentSamples.forEach((k,v) -> {
+				if(Math.abs(v - lastSamples.get().get(k)) > this.deltaDevices.get(k)) {
+					System.out.println("Carica su db!");				
+					if(v > this.thresholds.get(k).getY() || v < this.thresholds.get(k).getX()) {
+						System.out.println("Create out of range behavior");
+					}
+				}				
+			});
+		}
+		
+		this.lastSamples = Optional.of(new HashMap<>(currentSamples));		
+	}
+}
+
