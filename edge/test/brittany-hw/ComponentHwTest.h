7693916103eee865ff4077b13f63c72d65d3341c
Angelo Filaseta <filaseta.angelo@gmail.com>
2021-12-13 14:44:22 +0100

    Added HwComponent and its tests

---
+++ edge/test/brittany-hw/ComponentHwTest.h
@@ -0,0 +1,19 @@
+#ifndef BRITTANY_COMPONENT_HW_TEST_H
+#define BRITTANY_COMPONENT_HW_TEST_H
+
+#include <unity.h>
+#include "ComponentHw.h"
+
+#define HW_ID "component"
+
+ComponentHw mockComponent = ComponentHw(HW_ID);
+
+void test_get_component_hw_id() {
+    TEST_ASSERT_EQUAL_STRING(mockComponent.id().c_str(), HW_ID);
+}
+
+void test_ComponentHw(){
+    RUN_TEST(test_get_component_hw_id);
+}
+
+#endif //BRITTANY_COMPONENT_HW_TEST_H
