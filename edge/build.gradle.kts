e14caf2c453ac00a06fc28eb950d8d34988564a5
Angelo Filaseta <filaseta.angelo@gmail.com>
2021-12-13 14:48:37 +0100

    Added build gradle with base commands for platformio

---
+++ edge/build.gradle.kts
@@ -0,0 +1,15 @@
+tasks.register<Exec>("run") {
+    commandLine("pio", "run")
+}
+
+tasks.register<Exec>("test") {
+    commandLine("pio", "test")
+}
+
+tasks.register<Exec>("testRealHw") {
+    commandLine("pio", "test", "-e", "nodemcuv2")
+}
+
+tasks.register<Exec>("upload") {
+    commandLine("pio", "run", "-e", "nodemcuv2", "-t", "upload")
+}
