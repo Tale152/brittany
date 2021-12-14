70dd4f4c56d34454b1c2f0d0f7b73a43fe8cddda
Angelo Filaseta <filaseta.angelo@gmail.com>
2021-12-14 10:15:43 +0100

    Commented OperationHandlerResult
    Signed-off-by: Angelo Filaseta <filaseta.angelo@gmail.com>

--- edge/lib/brittany/src/operation-handler/OperationHandlerResult.h
+++ edge/lib/brittany/src/operation-handler/OperationHandlerResult.h
@@ -3,14 +3,33 @@
 
 #include "json/json.h"
 
+/**
+ * @brief A class that wraps the results information of an OperationHandler.
+ */
 class OperationHandlerResult {
 
 public:
 
+    /**
+     * @brief Construct a new Operation Handler Result object.
+     * 
+     * @param code the code of the result.
+     * @param content a json value containing information about the result.
+     */
     OperationHandlerResult(int code, Json::Value content);
 
+    /**
+     * @brief Return the result code.
+     * 
+     * @return int a code that identify what occurence happened executing the OperationHandler.
+     */
     int code();
 
+    /**
+     * @brief Return the Json value.
+     * 
+     * @return Json::Value a json value that contains information of what happened when executing the OperationHandler.
+     */
     Json::Value content();
 
 private:
