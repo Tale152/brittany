130280865ab6abea1e07bd078433a0e00176bf3f
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-15 23:08:16 +0100

    test: Block Organization creation on duplicate Organization name

--- auth-service/test/organization/create.test.js
+++ auth-service/test/organization/create.test.js
@@ -8,21 +8,47 @@ beforeAll((done) => db.createConnectionToTestDB(done))
 beforeEach(() => db.resetTestDB())
 afterAll((done) => db.dropConnectedTestDB(done))
 
-test("Checking for Organization creation", async () => {
+const organizationName = "TestOrganization"
+const name = "Peter"
+const surname = "Parker"
+const mail = "peter.parker@test.com"
+const birthdate = "1997-12-31"
+const password = "psw"
+const correctBody = {
+    organizationName: organizationName,
+    name: name,
+    surname: surname,
+    mail: mail,
+    birthdate: birthdate,
+    password: password
+}
+
+async function correctOrganizationCreation(){
     await rolesSetup.populateRoles()
     await supertest(server)
         .post("/organization/create")
-        .send({
-            organizationName: "TestOrganization",
-            name: "Peter",
-            surname: "Parker",
-            mail: "peter.parker@test.com",
-            birthdate: "1997-12-31",
-            password: "psw"
-        })
+        .send(correctBody)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(201)
         .then((res) => expect(res.body).toHaveProperty("token"))
-    }
-)
+}
+
+test("Correct Organization creation", correctOrganizationCreation)
+
+test("Block Organization creation on duplicate Organization name", async () => {
+    await correctOrganizationCreation()
+    await supertest(server)
+        .post("/organization/create")
+        .send({
+            organizationName: organizationName,
+            name: name,
+            surname: surname,
+            mail: "different." + mail,
+            birthdate: birthdate,
+            password: password
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(406)
+})
