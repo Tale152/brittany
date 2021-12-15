3c491ae2471da7afce524a5a6cb96d202791a500
Angelo Filaseta <filaseta.angelo@gmail.com>
2021-12-15 17:41:45 +0100

    Added thing descriptor template class

---
+++ edge/lib/brittany/src/thing-descriptor/ThingDescriptor.h
@@ -0,0 +1,12 @@
+#ifndef BRITTANY_THING_DESCRIPTOR_H
+#define BRITTANY_THING_DESCRIPTOR_H
+
+//TODO
+class ThingDescriptor{
+public:
+
+    std::string asString();
+};
+
+
+#endif //BRITTANY_THING_DESCRIPTOR_H
