1e7936a916191e41f37c173fc9dc69fc78c5b8d9
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-15 21:59:34 +0100

    created GET /organization/info

--- auth-service/src/routes/organization.js
+++ auth-service/src/routes/organization.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const tokenAuth = require('./controllers/tokenAuth')
+
 const PostCreateOrganization = require("./controllers/organization/post-create-organization")
+const GetOrganizationInfo = require("./controllers/organization/get-info")
 
 router.post("/organization/create", (req, res) => PostCreateOrganization.createOrganizationController(req, res))
+
+router.get("/organization/name", tokenAuth.verifyToken, (req, res) => GetOrganizationInfo.organizationInfoController(req, res))
 
 module.exports = router
