a8c052007a6c8470bab235112d99b1834bf1cd6c
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-15 19:19:15 +0100

    created new Organization and new Farmer instances in POST /organization/create

--- auth-service/src/routes/organization.js
+++ auth-service/src/routes/organization.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const Role = require('../mongoose/role')
 const Organization = require('../mongoose/organization')
+const OrganizationFactory = require('../mongoose/factories/organization')
 const Farmer = require('../mongoose/farmer')
+const FarmerFactory = require('../mongoose/factories/farmer')
 
 function isValidString(str){
     return str !== null && str !== undefined && str.trim() !== ""
@@ -18,7 +21,7 @@ function areOrganizationCreateParametersValid(params){
 }
 
 async function checkUniqueOrganizationCreateParameters(req, res){
-    Organization.findOne({ name: req.body.organizationName}).then(async result => {
+    Organization.findOne({ name: req.body.organizationName }).then(async result => {
         if(result !== null){
             res.status(406).json({err: "Organization name not available"})
         } else {
@@ -26,11 +29,33 @@ async function checkUniqueOrganizationCreateParameters(req, res){
                 if(result !== null){
                     res.status(406).json({err: "Mail not available"})
                 } else {
-                    res.status(201).json({token: "TODO"})
+                    createNewOrganization(req, res)
                 }
             }).catch(err => {
                 res.status(500).json({err: err})
             })
+        }
+    }).catch(err => {
+        res.status(500).json({err: err})
+    })
+}
+
+async function createNewOrganization(req, res){
+    Role.findOne({ name: "Owner" }).then(async ownerRole => {
+        if(ownerRole === null){
+            res.status(500).json({err: "Cannot find Owner role"})
+        } else {
+            var newOrganization = OrganizationFactory.createOrganization(req.body.organizationName)
+            var newFarmerOwner = FarmerFactory.createFarmer(
+                req.body.name,
+                req.body.surname,
+                req.body.birthdate,
+                req.body.mail,
+                req.body.password,
+                ownerRole._id,
+                newOrganization._id
+            )
+            res.status(201).json({token: "TODO"})
         }
     }).catch(err => {
         res.status(500).json({err: err})
