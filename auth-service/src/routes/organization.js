4e02f114582869f21fef03b000500d23911e1ba9
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-15 19:03:13 +0100

    added POST /organization/create parametes uniqueness

--- auth-service/src/routes/organization.js
+++ auth-service/src/routes/organization.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const Organization = require('../mongoose/organization')
+const Farmer = require('../mongoose/farmer')
+
 function isValidString(str){
     return str !== null && str !== undefined && str.trim() !== ""
 }
@@ -14,13 +17,32 @@ function areOrganizationCreateParametersValid(params){
         isValidString(params.birthdate)
 }
 
+async function checkUniqueOrganizationCreateParameters(req, res){
+    Organization.findOne({ name: req.body.organizationName}).then(async result => {
+        if(result !== null){
+            res.status(406).json({err: "Organization name not available"})
+        } else {
+            Farmer.findOne({ mail: req.body.mail }).then(async result => {
+                if(result !== null){
+                    res.status(406).json({err: "Mail not available"})
+                } else {
+                    res.status(201).json({token: "TODO"})
+                }
+            }).catch(err => {
+                res.status(500).json({err: err})
+            })
+        }
+    }).catch(err => {
+        res.status(500).json({err: err})
+    })
+}
+
 router.post("/organization/create", (req, res) => {
     if(!areOrganizationCreateParametersValid(req.body)){
         res.status(406).json({err: "Invalid parameters"})
     } else {
-        res.status(201).json({token: "TODO"})
+        checkUniqueOrganizationCreateParameters(req, res)
     }
-    
 })
 
 module.exports = router
