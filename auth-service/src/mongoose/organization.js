c119e5ea39fe2bf3e256b6d8d134ab9202e9a570
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-15 16:46:45 +0100

    created organization mongoose structure

---
+++ auth-service/src/mongoose/organization.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+let OrganizationSchema = new Schema({
+    name: {
+        type: String,
+        require: true,
+        unique: true
+    }
+})
+
+module.exports = mongoose.model("Organization", OrganizationSchema)
