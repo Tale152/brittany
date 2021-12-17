f76fcb41e858b0fca4195e0241d4676606d307e2
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-17 18:54:09 +0100

    created new route GET /farmer/info

--- auth-service/src/routes/farmer.js
+++ auth-service/src/routes/farmer.js
@@ -4,9 +4,12 @@ const router = express.Router()
 const tokenAuth = require('./controllers/tokenAuth')
 const GetFarmerLogin = require("./controllers/farmer/get-login")
 const PostFarmerRegister = require("./controllers/farmer/post-register")
+const GetFarmerInfo = require("./controllers/farmer/get-info")
 
 router.get("/farmer/login", GetFarmerLogin.farmerLoginController)
 
 router.post("/farmer/register", tokenAuth.verifyToken, PostFarmerRegister.farmerRegisterController)
+
+router.get("/farmer/info", tokenAuth.verifyToken, GetFarmerInfo.farmerInfoController)
 
 module.exports = router
