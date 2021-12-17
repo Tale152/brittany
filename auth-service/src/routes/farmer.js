ec0f654bff8d8cd60ccadaf24fdb9f1bf62ac6b5
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-17 18:33:36 +0100

    created farmer login route

--- auth-service/src/routes/farmer.js
+++ auth-service/src/routes/farmer.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const tokenAuth = require('./controllers/tokenAuth')
 const GetFarmerLogin = require("./controllers/farmer/get-login")
+const PostFarmerRegister = require("./controllers/farmer/post-register")
 
 router.get("/farmer/login", GetFarmerLogin.farmerLoginController)
 
-
+router.post("/farmer/register", tokenAuth.verifyToken, PostFarmerRegister.farmerRegisterController)
 
 module.exports = router
--- auth-service/src/routes/organization.js
+++ auth-service/src/routes/organization.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 const tokenAuth = require('./controllers/tokenAuth')
-
 const PostCreateOrganization = require("./controllers/organization/post-create")
 const GetOrganizationInfo = require("./controllers/organization/get-info")
 
