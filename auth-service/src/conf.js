532839268ad1652f9ddeca6ccd7350f1db657030
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-18 18:03:21 +0100

    created agentTokenAuth

--- auth-service/src/conf.js
+++ auth-service/src/conf.js
@@ -1,6 +1,7 @@
 module.exports = {
     serverPort: process.env.PORT || 81,
     farmerTokenSecret: process.env.FARMER_TOKEN_SECRET || "devFarmerTokenSecret",
+    agentTokenSecret: process.env.AGENT_TOKEN_SECRET || "devAgentTokenSecret",
     dbAddress: process.env.DB || 'mongodb://127.0.0.1/brittany_auth',
     testDbAddress: 'mongodb://127.0.0.1/brittany_auth',
     webClientAddress: process.env.WEB_CLIENT_ADDRESS || 'http://localhost:3000',
