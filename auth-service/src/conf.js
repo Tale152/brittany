f61ffa7d048f112f1e461d0e326d8cc3a611faf3
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-19 16:41:17 +0100

    changed test db connection string

--- auth-service/src/conf.js
+++ auth-service/src/conf.js
@@ -3,7 +3,7 @@ module.exports = {
     farmerTokenSecret: process.env.FARMER_TOKEN_SECRET || "devFarmerTokenSecret",
     agentTokenSecret: process.env.AGENT_TOKEN_SECRET || "devAgentTokenSecret",
     dbAddress: process.env.DB || 'mongodb://127.0.0.1/brittany_auth',
-    testDbAddress: 'mongodb://127.0.0.1/brittany_auth',
+    testDbAddress: 'mongodb://127.0.0.1/test_brittany_auth',
     asciiArt: 
         '    ____       _ __  __                                   __  __\n' +
         '   / __ )_____(_) /_/ /_____ _____  __  __   ____ ___  __/ /_/ /_\n' +
