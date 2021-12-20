aff555c73bb6111bc1481221d45a60b0c31067e9
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-12-20 18:39:36 +0100

    Implemented request of the authentication token to auth server

--- greenhouse_core/src/env/common/CommonArtifact.java
+++ greenhouse_core/src/env/common/CommonArtifact.java
@@ -12,21 +12,21 @@ import utility.Pair;
 import utility.Thresholds;
 
 public class CommonArtifact extends Artifact {
-	
+
 	private List<Device> devices;
 	private Thresholds thresholds;
-	
-	void init() {
-		devices = new ArrayList<>(Arrays.asList(
-					new Device("id1", "temperature"), 
-					new Device("id2", "light"),
-					new Device("id3", "temperature")
-				));
-		
+
+	void init() {}
+
+	@OPERATION
+	void initAfterAuthentication() {
+		devices = new ArrayList<>(Arrays.asList(new Device("id1", "temperature"), new Device("id2", "light"),
+				new Device("id3", "temperature")));
+
 		thresholds = new Thresholds();
-		thresholds.addThreshold("temperature", new Pair<>(1000,3000));
-		thresholds.addThreshold("light", new Pair<>(1000,3000));
-		
-		defineObsProperty("setup", devices, thresholds);
+		thresholds.addThreshold("temperature", new Pair<>(1000, 3000));
+		thresholds.addThreshold("light", new Pair<>(1000, 3000));
+
+		//defineObsProperty("setup", devices, thresholds);
 	}
 }
---
+++ greenhouse_core/src/env/communication/ServiceArtifact.java
@@ -0,0 +1,79 @@
+// CArtAgO artifact code for project greenhouse_core
+
+package communication;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import cartago.*;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class ServiceArtifact extends Artifact {
+
+	private final static String LOGIN_FILE = "login.txt";
+	
+	private OkHttpClient client;
+	private List<String> loginData;
+
+	void init() {
+		this.loginData = new ArrayList<>();
+		this.client = new OkHttpClient();
+	}
+
+	@OPERATION void retrieveAuthenticationData() {
+		try {
+			File file = new File(LOGIN_FILE);
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String data = scanner.nextLine();
+				this.loginData.add(data);
+			}
+			scanner.close();
+			autheticate();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@INTERNAL_OPERATION void autheticate() {
+		HttpUrl.Builder urlBuilder = HttpUrl.parse("http://localhost:81/agent/login").newBuilder();
+		urlBuilder.addQueryParameter("organizationName", this.loginData.get(0));
+		urlBuilder.addQueryParameter("greenhouseName", this.loginData.get(1));
+		urlBuilder.addQueryParameter("environmentName", this.loginData.get(2));
+		urlBuilder.addQueryParameter("environmentPassword", this.loginData.get(3));
+		String url = urlBuilder.build().toString();
+		
+		Request request = new Request.Builder()
+                .url(url)
+                .build();
+		
+		try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+            	throw new IOException("Unexpected code " + response);
+            }
+            
+            JsonObject tokenObject = JsonParser.parseString(response.body().string()).getAsJsonObject(); 
+            
+            if (tokenObject.has("token")) {
+            	System.out.println("TOKEN:" + tokenObject.get("token"));
+            } else {
+            	throw new IllegalStateException("You did not received the token.");
+            }
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
