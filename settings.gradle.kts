4dfaea74b214cab93873c594a2f115e075b7eb7b
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-21 22:12:46 +0100

    added subproject in root settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,6 @@ rootProject.name = "brittany"
 include("persistence-service")
 include("auth-service")
 include("settings-service")
+include("web-client")
+include("edge")
+include("greenhouse_core")
