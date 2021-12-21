8f2cbde39e5fe575223c969a8dfa2dc3bd7c5148
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-21 21:07:07 +0100

    removed subproject from root settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,4 @@ rootProject.name = "brittany"
 
 include("persistence-service")
 include("auth-service")
-include("greenhouse_core")
-include("web-client")
-include("edge")
 include("settings-service")
