929e4a12fc962f39d65f3b3a607a0c2293ce1fdc
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-21 17:36:39 +0100

    created GET /temperature/latest

--- persistence-service/src/routes/temperature.js
+++ persistence-service/src/routes/temperature.js
@@ -4,7 +4,10 @@ const router = express.Router()
 const agentTokenAuth = require('./controllers/agentTokenAuth')
 
 const PostTemperatureRegister = require("./controllers/temperature/post-register")
+const GetTemperatureLatest = require("./controllers/temperature/get-latest")
 
 router.post("/temperature/register", agentTokenAuth.verifyToken, PostTemperatureRegister.temperatureRegisterController)
+
+router.get("/temperature/latest", agentTokenAuth.verifyToken, GetTemperatureLatest.temperatureLatestController)
 
 module.exports = router
