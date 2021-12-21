618ad3e51b5d520019bc315c7b437dc9daf46c37
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-21 11:46:31 +0100

    created GET /settings/latest

--- settings-service/src/routes/settings.js
+++ settings-service/src/routes/settings.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const farmerTokenAuth = require('./controllers/farmerTokenAuth')
+const agentTokenAuth = require('./controllers/agentTokenAuth')
 const PostCreateSettings = require("./controllers/settings/post-create")
+const GetLatestSettings = require("./controllers/settings/get-latest")
 
 router.post("/settings/create", farmerTokenAuth.verifyToken, PostCreateSettings.createSettingsController)
+
+router.get("/settings/latest", agentTokenAuth.verifyToken, GetLatestSettings.latestSettingsController)
 
 module.exports = router
