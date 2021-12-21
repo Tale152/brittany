6ae58bf572d404e209603263211770cc1fdf2f1d
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-21 16:30:57 +0100

    changed server configuration

--- persistence-service/src/conf.js
+++ persistence-service/src/conf.js
@@ -1,8 +1,9 @@
 module.exports = {
     serverPort: process.env.PORT || 80,
+    farmerTokenSecret: process.env.FARMER_TOKEN_SECRET || "devFarmerTokenSecret",
+    agentTokenSecret: process.env.AGENT_TOKEN_SECRET || "devAgentTokenSecret",
     dbAddress: process.env.DB || 'mongodb://127.0.0.1/brittany_persistence',
-    testDbAddress: 'mongodb://127.0.0.1/brittany_persistence_test',
-    webClientAddress: process.env.WEB_CLIENT_ADDRESS || 'http://localhost:3000',
+    testDbAddress: 'mongodb://127.0.0.1/test_brittany_persistence',
     asciiArt: 
         '######\n' +                                                                                                                                                                   
         '#     # #####  # ##### #####   ##   #    # #   #    #####  ###### #####   ####  #  ####  ##### ###### #    #  ####  ######\n' +
