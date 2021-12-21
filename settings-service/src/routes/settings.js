529feaf5539d44c29aad76cfb75687c4fd8fa855
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-21 12:04:54 +0100

    created POST /settings/latestActiveState

--- settings-service/src/routes/settings.js
+++ settings-service/src/routes/settings.js
@@ -4,10 +4,13 @@ const router = express.Router()
 const farmerTokenAuth = require('./controllers/farmerTokenAuth')
 const agentTokenAuth = require('./controllers/agentTokenAuth')
 const PostCreateSettings = require("./controllers/settings/post-create")
+const PostLatestActiveStateSettings = require("./controllers/settings/post-latestActiveState")
 const GetListSettings = require("./controllers/settings/get-list")
 const GetLatestSettings = require("./controllers/settings/get-latest")
 
 router.post("/settings/create", farmerTokenAuth.verifyToken, PostCreateSettings.createSettingsController)
+
+router.post("/settings/latestActiveState", farmerTokenAuth.verifyToken, PostLatestActiveStateSettings.latestActiveStateSettingsController)
 
 router.get("/settings/list", farmerTokenAuth.verifyToken, GetListSettings.listSettingsController)
 
