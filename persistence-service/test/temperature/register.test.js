6d40a5571b29eeafc825162bf24bf0769c726395
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-23 11:42:19 +0100

    expanded temperature register tests

--- persistence-service/test/temperature/register.test.js
+++ persistence-service/test/temperature/register.test.js
@@ -20,17 +20,69 @@ const correctBody = {
     timestamp: timestamp
 }
 
-async function temperatureRegister(body, code, then){
+async function temperatureRegister(body, token, code, then){
     await supertest(server)
         .post("/temperature/register")
         .send(body)
         .set('token', token)
         .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
         .expect(code)
         .then((res) => then(res))
 }
 
 test("Correct Temperature registration", async () => {
-    await temperatureRegister(correctBody, 201, (res) => expect(res.body).toHaveProperty("id"))
+    await temperatureRegister(correctBody, token, 201, (res) => expect(res.body).toHaveProperty("id"))
+})
+
+test("Wrong token", async () => {
+    await temperatureRegister(correctBody, jwt.sign({}, "wrong"), 401, (res) => {/* does nothing */})
+})
+
+test("Missing id", async () => {
+    await temperatureRegister(
+        {
+            value: value,
+            timestamp: timestamp
+        },
+        token,
+        406,
+        (res) => expect(res.body).toHaveProperty("err")
+    )
+})
+
+test("Missing value", async () => {
+    await temperatureRegister(
+        {
+            id: idEnvironment,
+            timestamp: timestamp
+        },
+        token,
+        406,
+        (res) => expect(res.body).toHaveProperty("err")
+    )
+})
+
+test("Value not numeric", async () => {
+    await temperatureRegister(
+        {
+            id: idEnvironment,
+            value: "not a number",
+            timestamp: timestamp
+        },
+        token,
+        406,
+        (res) => expect(res.body).toHaveProperty("err")
+    )
+})
+
+test("Missing timestamp", async () => {
+    await temperatureRegister(
+        {
+            id: idEnvironment,
+            value: value
+        },
+        token,
+        406,
+        (res) => expect(res.body).toHaveProperty("err")
+    )
 })
