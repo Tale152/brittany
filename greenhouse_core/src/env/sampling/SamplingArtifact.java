a240fc0c49a8a638926ba724a92df1630aaf2de8
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-12-25 00:08:33 +0100

    Fixed stream to filter the list of devices to use to sample

--- greenhouse_core/src/env/sampling/SamplingArtifact.java
+++ greenhouse_core/src/env/sampling/SamplingArtifact.java
@@ -6,11 +6,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import cartago.*;
 import utility.Device;
 import utility.Thresholds;
 
+/**
+ * SamplingArtifact is an Artifact used to handle all the sampling operations of the application.
+ * Once everything is set up, every samplingAgent that is focusing on this Artifact will start the sampling operation,
+ * and when the samples are retried, the update of the information are going to be handled here (uploading the data
+ * to the persistence service and starting an actuator if needed).
+ *
+ */
 public class SamplingArtifact extends Artifact {
 	
 	private static final int DELTA = 1;
@@ -24,15 +33,29 @@ public class SamplingArtifact extends Artifact {
 		this.lastSamples = Optional.empty();
 	}
 	
+	/**
+	 * Operation used to set up the devices and the thresholds, which are the basic information
+	 * needed to actually start the sampling.
+	 * @param devices a list of all the devices that are present in the greenhouse.
+	 * @param thresholds the minimum and maximum values that a device of a specific role must observe. If it is 
+	 * not respected, an actuator is started to resolve the bad status.
+	 */
 	@OPERATION void setup(final List<Device> devices, final Thresholds thresholds) {
 		this.thresholds = thresholds;
 		this.devices = devices;
 		System.out.println("Init " + this.devices + " " + this.thresholds);
 	}
 	
+	/**
+	 * Operation used to actually perform a sample. A specific samplingAgent, every delta of time, start a sample operation
+	 * for a specific role of devices. This starts the communication with the devices performed by the CommunicationAgent.
+	 * @param role, every SamplingAgent have a specific role, that matches with the device role. It is the type of 
+	 * sample that is going to be retrieved.
+	 */
 	@OPERATION void sampleOperation(final String role) {
 		//the role can be use to filter the devices to communicate with
-		defineObsProperty("communicate", this.devices); //you can send here the devices you want to ask data to
+		defineObsProperty("communicate",
+				this.devices.stream().filter(d -> d.getRole().equals(role)).collect(Collectors.toList())); 
 	}
 	
 	@OPERATION void updateOperation(final List<Device> currentSamples) {
