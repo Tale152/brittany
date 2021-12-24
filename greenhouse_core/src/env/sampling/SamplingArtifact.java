f15c48e14d9a95608e2e9e422bc3405217a9ab76
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-12-25 00:12:06 +0100

    Removed unused import in SamplingArtifact

--- greenhouse_core/src/env/sampling/SamplingArtifact.java
+++ greenhouse_core/src/env/sampling/SamplingArtifact.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import cartago.*;
