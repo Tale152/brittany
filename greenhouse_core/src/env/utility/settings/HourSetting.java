9c1a37aac1a288e4729a3bc86446e8cbb8bcbe00
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-12-27 15:45:50 +0100

    Refactor thresholds as setting to handle it better

---
+++ greenhouse_core/src/env/utility/settings/HourSetting.java
@@ -0,0 +1,29 @@
+package utility.settings;
+
+import java.time.LocalTime;
+
+public class HourSetting extends Setting {
+	
+	private final LocalTime fromTime;
+	private final LocalTime toTime;
+	
+	public HourSetting(final String category, final LocalTime fromTime, final LocalTime toTime) {
+		super(category);
+		this.fromTime = fromTime;
+		this.toTime = toTime;
+	}
+
+	public LocalTime getFromTime() {
+		return fromTime;
+	}
+
+	public LocalTime getToTime() {
+		return toTime;
+	}
+
+	@Override
+	public String toString() {
+		return "HourSetting [fromTime=" + fromTime + ", toTime=" + toTime + " " + super.toString() +  "]";
+	}
+
+}
---
+++ greenhouse_core/src/env/utility/settings/RangeSetting.java
@@ -0,0 +1,23 @@
+package utility.settings;
+
+import utility.Pair;
+
+public class RangeSetting extends Setting {
+	
+	private Pair<Integer, Integer> range;
+	
+	public RangeSetting(final String category, final Pair<Integer, Integer> range) {
+		super(category);
+		this.range = range;
+	}
+	
+	public Pair<Integer, Integer> getRange(){
+		return this.range;
+	}
+
+	@Override
+	public String toString() {
+		return "RangeSettings [range =" + this.range + " " + super.toString() + "] ";
+	}
+
+}
