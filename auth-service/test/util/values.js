0a09b5c6976b61311333b60499559b572c992ebd
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-27 17:59:49 +0100

    created values util in tests

---
+++ auth-service/test/util/values.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+
+const organizationName = "TestOrganization"
+const name = "Peter"
+const surname = "Parker"
+const mail = "peter.parker@test.com"
+const birthdate = "1962-06-05"
+const password = "psw"
+const correctOrganizationBody = {
+    organizationName: organizationName,
+    name: name,
+    surname: surname,
+    mail: mail,
+    birthdate: birthdate,
+    password: password
+}
+
+module.exports = {
+    organizationName: organizationName,
+    name: name,
+    surname: surname,
+    mail: mail,
+    birthdate: birthdate,
+    password: password,
+    correctOrganizationBody: correctOrganizationBody,
+    correctAgentToken: jwt.sign({}, require('../../src/conf').agentTokenSecret),
+    correctFarmerToken: jwt.sign({}, require('../../src/conf').farmerTokenSecret),
+    wrongToken: jwt.sign({}, "wrong")
+}
