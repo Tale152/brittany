8ad5792be0d58a986ba58d0a7f096af1fbd4b83f
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-12-27 16:32:21 +0100

    Fixed JavaDoc in ActautionArtifact

--- greenhouse_core/src/env/actuation/ActuationArtifact.java
+++ greenhouse_core/src/env/actuation/ActuationArtifact.java
@@ -9,17 +9,18 @@ import utility.settings.RangeSetting;
 
 /**
  * ActautionArtifact is an Artifact that is used to send a message to an actuator to 
- * handle when the current sample registered is bigger or lower than the threshold set for
- * a specific device role.
+ * handle when the current sample registered is bigger or lower than the setting of
+ * a specific device category.
  *
  */
 public class ActuationArtifact extends Artifact {
+	
 	void init(int initialValue) {}
 
 	/**
 	 * Used to check the current sample and to notify if it is bigger or lower that the threshold.
-	 * @param outOfRangeDevice, the device that registered the out of range sample.
-	 * @param threshold, the threshold of the specific role of the device.
+	 * @param outOfRangeDevice the device that registered the out of range sample.
+	 * @param setting the setting of the specific category of the device.
 	 */
 	@OPERATION void actuate(Device outOfRangeDevice, RangeSetting setting) {
 		int currentValue = outOfRangeDevice.getCurrentValue();
