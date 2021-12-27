46799ca3b4a62d488cf59548fc5d30bd2ac238f1
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-27 18:00:15 +0100

    simplified test

--- auth-service/test/organization/post.create.test.js
+++ auth-service/test/organization/post.create.test.js
@@ -1,44 +1,30 @@
 const server = require('../../src/server')
 const httpTest = require('../util/httpTest')
 const db = require('../util/db')
+const values = require('../util/values')
 
 beforeAll((done) => db.createConnectionToTestDB(done))
 beforeEach(() => db.resetTestDB())
 afterAll((done) => db.dropConnectedTestDB(done))
-
-const organizationName = "TestOrganization"
-const name = "Peter"
-const surname = "Parker"
-const mail = "peter.parker@test.com"
-const birthdate = "1997-12-31"
-const password = "psw"
-const correctBody = {
-    organizationName: organizationName,
-    name: name,
-    surname: surname,
-    mail: mail,
-    birthdate: birthdate,
-    password: password
-}
 
 async function organizationCreate(body, code, then){
     await httpTest.post(server, "/organization/create", body, "", code, then)
 }
 
 test("Correct Organization creation", async () => {
-    await organizationCreate(correctBody, 201, (res) => expect(res.body).toHaveProperty("token"))
+    await organizationCreate(values.correctOrganizationBody, 201, (res) => expect(res.body).toHaveProperty("token"))
 })
 
 test("Block Organization creation on duplicate Organization name", async () => {
-    await organizationCreate(correctBody, 201, (res) => expect(res.body).toHaveProperty("token"))
+    await organizationCreate(values.correctOrganizationBody, 201, (res) => expect(res.body).toHaveProperty("token"))
     await organizationCreate(
         {
-            organizationName: organizationName,
-            name: name,
-            surname: surname,
-            mail: "different." + mail,
-            birthdate: birthdate,
-            password: password
+            organizationName: values.organizationName,
+            name: values.name,
+            surname: values.surname,
+            mail: "different." + values.mail,
+            birthdate: values.birthdate,
+            password: values.password
         },
         406,
         (res) => expect(res.body).toHaveProperty("err")
@@ -46,15 +32,15 @@ test("Block Organization creation on duplicate Organization name", async () => {
 })
 
 test("Block Organization creation on duplicate Farmer mail", async () => {
-    await organizationCreate(correctBody, 201, (res) => expect(res.body).toHaveProperty("token"))
+    await organizationCreate(values.correctOrganizationBody, 201, (res) => expect(res.body).toHaveProperty("token"))
     await organizationCreate(
         {
-            organizationName: "different " + organizationName,
-            name: name,
-            surname: surname,
-            mail: mail,
-            birthdate: birthdate,
-            password: password
+            organizationName: "different " + values.organizationName,
+            name: values.name,
+            surname: values.surname,
+            mail: values.mail,
+            birthdate: values.birthdate,
+            password: values.password
         },
         406,
         (res) => expect(res.body).toHaveProperty("err")
