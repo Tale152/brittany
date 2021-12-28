29bb036283d5f45289d5c883b40a3a8932eefb55
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-28 11:04:52 +0100

    fixed block warning

--- web-client/src/react/_common/form/button/FormButton.js
+++ web-client/src/react/_common/form/button/FormButton.js
@@ -3,14 +3,14 @@ import Button from 'react-bootstrap/Button'
 export default function FormButton(props){
     return (
         <Button
-            style={props.isLoading ? { pointerEvents: "none", opacity: "0.4" } : {}}
-            className="col-lg-12 mt-3"
-            variant="primary"
-            size="lg"
-            block
-            onClick={props.onClick}
+            style = { props.isLoading ? { pointerEvents: "none", opacity: "0.4" } : {} }
+            className = "col-lg-12 mt-3"
+            variant = "primary"
+            size = "lg"
+            block = "true"
+            onClick = { props.onClick }
         >
-            {props.text}
+            { props.text }
         </Button>
     )
 }
