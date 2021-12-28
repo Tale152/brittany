c7dcb91c65250eb22401959640564700a303aea6
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-28 15:25:21 +0100

    working registration

---
+++ web-client/src/js/organizationRegister.js
@@ -0,0 +1,32 @@
+import $ from 'jquery'
+
+import { setToken } from '../redux/identity/actions'
+import { setIsLoading } from '../redux/util/actions'
+
+$.ajaxSetup({
+    contentType: "application/json; charset=utf-8"
+})
+
+export default function organizationRegister(organizationName, name, surname, mail, birthdate, password, passwordConfirm, dispatch){
+    dispatch(setIsLoading(true))
+    $.post("http://localhost:81/organization/create", createParams(organizationName, name, surname, mail, birthdate, password))
+        .done(function (result) {
+            dispatch(setToken(result.token))
+        })
+        .fail(function (result) {
+            console.log("fail")
+        }).always(function() {
+            dispatch(setIsLoading(false))
+        })
+}
+
+function createParams(organizationName, name, surname, mail, birthdate, password) {
+    return JSON.stringify({
+        organizationName: organizationName.trim(),
+        name: name.trim(),
+        surname: surname.trim(),
+        mail: mail.trim(),
+        birthdate: birthdate.trim(),
+        password: password.trim()
+    })
+}
--- web-client/src/react/register/components/RegisterPage.js
+++ web-client/src/react/register/components/RegisterPage.js
@@ -8,14 +8,19 @@ import FormPassword from '../../_common/form/input/FormPassword'
 import FormLogin from '../../_common/form/button/FormLogin'
 import RedirectButton from '../../_common/navigation/RedirectButton'
 
-import farmerLogin from '../../../js/farmerLogin'
+import organizationRegister from '../../../js/organizationRegister'
 
 export default function RegisterPage() {
 
     const dispatch = useDispatch()
     const [goToFarmerLogin, setGoToFarmerLogin] = useState(false)
-    const [inputMail, setInputMail] = useState(undefined)
-    const [inputPassword, setInputPassword] = useState(undefined)
+    const [inputOrganizationName, setOrganizationName] = useState("")
+    const [inputName, setInputName] = useState("")
+    const [inputSurname, setInputSurname] = useState("")
+    const [inputMail, setInputMail] = useState("")
+    const [inputBirthdate, setInputBirthdate] = useState("")
+    const [inputPassword, setInputPassword] = useState("")
+    const [inputPasswordConfirm, setInputPasswordConfirm] = useState("")
 
     return (
         <div role="main" style={{ overflowX: "hidden" }}>
@@ -24,21 +29,51 @@ export default function RegisterPage() {
                     <Form className="mt-1 mb-3"  >
                         <h1 className="d-flex justify-content-center">Register</h1>
                         <FormText
+                            text = { "Organization's name:" }
+                            placeholder = { "Enter Organization's name" }
+                            onChange = { input => setOrganizationName(input) }
+                            onEnter = { () => organizationRegister(inputOrganizationName, inputName, inputSurname, inputMail, inputBirthdate, inputPassword, inputPasswordConfirm, dispatch) }
+                        />
+                        <FormText
+                            text = { "Name:" }
+                            placeholder = { "Enter your name" }
+                            onChange = { input => setInputName(input) }
+                            onEnter = { () => organizationRegister(inputOrganizationName, inputName, inputSurname, inputMail, inputBirthdate, inputPassword, inputPasswordConfirm, dispatch) }
+                        />
+                        <FormText
+                            text = { "Surname:" }
+                            placeholder = { "Enter your surname" }
+                            onChange = { input => setInputSurname(input) }
+                            onEnter = { () => organizationRegister(inputOrganizationName, inputName, inputSurname, inputMail, inputBirthdate, inputPassword, inputPasswordConfirm, dispatch) }
+                        />
+                        <FormText
                             text = { "Mail:" }
-                            placeholder = { "Enter Mail" }
+                            placeholder = { "Enter your mail" }
                             onChange = { input => setInputMail(input) }
-                            onEnter = { () => farmerLogin(inputMail, inputPassword, dispatch) }
+                            onEnter = { () => organizationRegister(inputOrganizationName, inputName, inputSurname, inputMail, inputBirthdate, inputPassword, inputPasswordConfirm, dispatch) }
+                        />
+                        <FormText
+                            text = { "Birthdate:" }
+                            placeholder = { "Enter your birthdate" }
+                            onChange = { input => setInputBirthdate(input) }
+                            onEnter = { () => organizationRegister(inputOrganizationName, inputName, inputSurname, inputMail, inputBirthdate, inputPassword, inputPasswordConfirm, dispatch) }
                         />
                         <FormPassword
                             text = { "Password:" }
-                            placeholder = { "Enter Password" }
+                            placeholder = { "Create a password" }
                             onChange = { input => setInputPassword(input) }
-                            onEnter = { () => farmerLogin(inputMail, inputPassword, dispatch) }
+                            onEnter = { () => organizationRegister(inputOrganizationName, inputName, inputSurname, inputMail, inputBirthdate, inputPassword, inputPasswordConfirm, dispatch) }
+                        />
+                        <FormPassword
+                            text = { "Confirm password:" }
+                            placeholder = { "Confirm password" }
+                            onChange = { input => setInputPasswordConfirm(input) }
+                            onEnter = { () => organizationRegister(inputOrganizationName, inputName, inputSurname, inputMail, inputBirthdate, inputPassword, inputPasswordConfirm, dispatch) }
                         />
                         <div className="mt-lg-3">
                             <FormLogin
-                                text={ "Login" } 
-                                onClick = { () => farmerLogin(inputMail, inputPassword, dispatch) }
+                                text={ "Register" } 
+                                onClick = { () => organizationRegister(inputOrganizationName, inputName, inputSurname, inputMail, inputBirthdate, inputPassword, inputPasswordConfirm, dispatch) }
                             />
                             <RedirectButton
                                 text = { "Login" }
