0248d5d292eb436fe98b9566025f8bdfb4703dc5
Angelo Filaseta <filaseta.angelo@gmail.com>
2021-12-30 15:11:44 +0100

    main is now more clean as diffenent edges are in other files

--- edge/src/main.cpp
+++ edge/src/main.cpp
@@ -8,37 +8,15 @@
 #include "temp-hum-sensor/hw/DHT22SensorHw.h"
 #include "modules/DHT22Module.h"
 #include "modules/Module.h"
+#include "modules/dht22.h"
+#include "modules/mock-digital-light.h"
 
-
-std::list<Module*> modules;
 Esp8266WebServer* server;
 
-
-#ifdef BRITTANY_MAIN_MOCK_DIGITAL_LIGHT
-
 void setup_variables() {
-    MockDigitalLightHw* light0 = new MockDigitalLightHw("0", 0);
-    MockDigitalLightHw* light1 = new MockDigitalLightHw("1", 1);
-    std::list<MockDigitalLightHw*> lights = std::list<MockDigitalLightHw*>({light0, light1});
-    modules.push_back(new MockDigitalLightModule("light", lights));
-    Edge* edge = new Edge(modules);
+    Edge* edge = new Edge(edge_modules());
     server = new Esp8266WebServer(edge);
 }
-
-#endif
-
-
-#ifdef BRITTANY_MAIN_DHT22
-
-void setup_variables() {
-    DHT22SensorHw* dht22 = new DHT22SensorHw("dht22", D5);
-    std::list<DHT22SensorHw*> sensors = std::list<DHT22SensorHw*>({dht22});
-    modules.push_back(new DHT22Module("temp-hum", sensors));
-    Edge* edge = new Edge(modules);
-    server = new Esp8266WebServer(edge);
-}
-
-#endif
 
 void connect_to_wifi() {
     WiFi.begin(WIFI_SSID, WIFI_PSWD);
