f052bc03602c14639a532512485fc1e4ba45f43a
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-31 17:15:46 +0100

    created http call farmerRegister

---
+++ web-client/src/js/farmer/farmerRegister.js
@@ -0,0 +1,33 @@
+import $ from 'jquery'
+
+import { setIsLoading } from '../../redux/util/actions'
+import farmerList from './farmerList'
+
+export default function farmerRegister(token, name, surname, mail, birthdate, password, passwordConfirm, dispatch){
+    dispatch(setIsLoading(true))
+    $.ajaxSetup({
+        contentType: "application/json; charset=utf-8",
+        headers:{
+           'token': token
+        }
+    })
+    //TODO check password
+    $.post("http://localhost:81/farmer/register", createParams(name, surname, mail, birthdate, password))
+        .done(function (result) {
+            farmerList(token, dispatch)
+        })
+        .fail(function (result) {
+            console.log("fail")
+            dispatch(setIsLoading(false))
+        })
+}
+
+function createParams(name, surname, mail, birthdate, password) {
+    return JSON.stringify({
+        name: name,
+        surname: surname,
+        mail: mail,
+        birthdate: birthdate,
+        password: password
+    })
+}
