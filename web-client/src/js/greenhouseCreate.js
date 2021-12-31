bdd2d48f4cccea3210a0fb34cb0fe648d391e5a8
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-31 13:55:43 +0100

    created creenhouseCreate http call

---
+++ web-client/src/js/greenhouseCreate.js
@@ -0,0 +1,28 @@
+import $ from 'jquery'
+
+import { setIsLoading } from '../redux/util/actions'
+import greenhouseList from './greenhouseList'
+
+export default function greenhouseCreate(token, name, dispatch){
+    dispatch(setIsLoading(true))
+    $.ajaxSetup({
+        contentType: "application/json; charset=utf-8",
+        headers:{
+           'token': token
+        }
+    })
+    $.post("http://localhost:81/greenhouse/create", createParams(name))
+        .done(function (result) {
+            greenhouseList(token, dispatch)
+        })
+        .fail(function (result) {
+            console.log("fail")
+            dispatch(setIsLoading(false))
+        })
+}
+
+function createParams(name) {
+    return JSON.stringify({
+        name: name
+    })
+}
