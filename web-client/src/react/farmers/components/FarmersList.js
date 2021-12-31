889923de8745c59b5bf6e8f2a35ac363534e03e4
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-31 18:02:53 +0100

    created FarmersListModal

--- web-client/src/react/farmers/components/FarmersList.js
+++ web-client/src/react/farmers/components/FarmersList.js
@@ -1,12 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
 
 import { setSelectedFarmer } from '../../../redux/farmers/actions'
+import FarmersListModal from './FarmersListModal'
 import List from '../../_common/List'
 
 export default function FarmersList() {
 
     const dispatch = useDispatch()
     let farmers = useSelector(state => state.farmers.list)
+    const [showModal, setShowModal] = useState(false)
 
     return (
         <>
@@ -14,8 +17,10 @@ export default function FarmersList() {
                 elements={farmers}
                 onElementClick={(id) => {
                     dispatch(setSelectedFarmer(id))
+                    setShowModal(true)
                 }}
             />
+            <FarmersListModal show = { showModal } onHide = { () => setShowModal(false) } />
         </>
     )
 }
