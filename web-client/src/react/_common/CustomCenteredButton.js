f95d368c1cbdddaf230872fe154e0a73233817bf
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-31 13:40:22 +0100

    DRY: created CustomCenteredButton

---
+++ web-client/src/react/_common/CustomCenteredButton.js
@@ -0,0 +1,18 @@
+import { useSelector } from 'react-redux'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+
+export default function CustomCenteredButton(props){
+    return (
+        <Row className="d-flex justify-content-center">
+            <Button
+                style = { useSelector(state => state.util.isLoading) ? { pointerEvents: "none", opacity: "0.4" } : {} }
+                className = "col-lg-3 col-12 my-2"
+                variant = "primary"
+                onClick = {props.onClick}
+            >
+                {props.text}
+            </Button>
+        </Row>
+    )
+}
