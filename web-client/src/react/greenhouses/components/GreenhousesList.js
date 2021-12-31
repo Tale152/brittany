898dfcbb6dac235876232ea8de20ead03997a68f
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-31 14:43:31 +0100

    simplified GreenhousePage

--- web-client/src/react/greenhouses/components/GreenhousesList.js
+++ web-client/src/react/greenhouses/components/GreenhousesList.js
@@ -1,7 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
 
 import environmentList from '../../../js/environment/environmentList'
 import { setSelectedGreenhouse } from '../../../redux/greenhouses/actions'
+import EnvironmentListModal from './EnvironmentListModal'
 import List from '../../_common/List'
 
 export default function GreenhousesList(props) {
@@ -9,15 +11,19 @@ export default function GreenhousesList(props) {
     const dispatch = useDispatch()
     let token = useSelector(state => state.identity.token)
     let greenhouses = useSelector(state => state.greenhouses.list)
+    const [showEnvironmentModal, setShowEnvironmentModal] = useState(false)
 
     return (
-        <List
-            elements={greenhouses}
-            onElementClick={(id) => {
-                dispatch(setSelectedGreenhouse(id))
-                environmentList(token, id, dispatch)
-                props.showEnvironmentModal()
-            }}
-        />
+        <>
+            <List
+                elements={greenhouses}
+                onElementClick={(id) => {
+                    dispatch(setSelectedGreenhouse(id))
+                    environmentList(token, id, dispatch)
+                    setShowEnvironmentModal(true)
+                }}
+            />
+            <EnvironmentListModal show = { showEnvironmentModal } onHide = { () => setShowEnvironmentModal(false) } />
+        </>
     )
 }
--- web-client/src/react/greenhouses/components/GreenhousesPage.js
+++ web-client/src/react/greenhouses/components/GreenhousesPage.js
@@ -1,15 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import greenhouseList from '../../../js/greenhouse/greenhouseList'
 import CreateGreenhouseButton from './CreateGreenhouseButton'
 import GreenhousesList from './GreenhousesList'
-import EnvironmentListModal from './EnvironmentListModal'
 
 export default function GreenhousesPage() {
 
     const dispatch = useDispatch()
-    const [showEnvironmentModal, setShowEnvironmentModal] = useState(false)
     let token = useSelector(state => state.identity.token)
 
     useEffect(() => greenhouseList(token, dispatch), [token, dispatch])
@@ -18,8 +16,7 @@ export default function GreenhousesPage() {
         <div role="main" style={{ overflowX: "hidden" }}>
             <h1 className="d-flex justify-content-center">Greenhouses</h1>
             <CreateGreenhouseButton />
-            <GreenhousesList showEnvironmentModal = { () => setShowEnvironmentModal(true) } />
-            <EnvironmentListModal show = { showEnvironmentModal } onHide = { () => setShowEnvironmentModal(false) } />
+            <GreenhousesList />
         </div>   
     )
   
