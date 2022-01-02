8d6a57df8aa25c2eedf5d38c8872c7ec162d73c7
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-01-02 17:33:59 +0100

    Added Type enum

--- edge/lib/brittany/src/operation-handler/types/OperationType.h
+++ edge/lib/brittany/src/operation-handler/types/OperationType.h
@@ -1,6 +1,10 @@
 #ifndef BRITTANY_OPERATION_TYPE_H
 #define BRITTANY_OPERATION_TYPE_H
 
+/**
+ * @brief Enum that describes the OperationHandler type. The type will be used for the
+ * Thing Descriptor creation.
+ */
 enum OperationType {
     PROPERTY,
     ACTION,
