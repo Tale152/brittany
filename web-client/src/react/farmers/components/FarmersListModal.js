61a59c956a2e436f2d3154fde597e1758ebec164
Tale152 <alessandro.talmi@studio.unibo.it>
2022-01-02 09:39:54 +0100

    fixed selected farmer problem

--- web-client/src/react/farmers/components/FarmersListModal.js
+++ web-client/src/react/farmers/components/FarmersListModal.js
@@ -1,16 +1,19 @@
+import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
 
+import { setSelectedFarmer } from '../../../redux/farmers/actions'
 import CustomModal from '../../_common/CustomModal'
 
 export default function FarmersListModal(props) {
 
+    const dispatch = useDispatch()
     let farmers = useSelector(state => state.farmers)
 
     return (
         <CustomModal
             show={props.show}
             onHide={() => {
-                
+                dispatch(setSelectedFarmer(null))
                 props.onHide()
             }}
             title={"Farmer info"}
@@ -20,13 +23,17 @@ export default function FarmersListModal(props) {
 }
 
 function createFarmerInfo(farmers){
-    let selected = farmers.list.find(f => f._id === farmers.selected)
-    return (
-        <div>
-            <p>Name: {selected.name}</p>
-            <p>Surname: {selected.surname}</p>
-            <p>Mail: {selected.mail}</p>
-            <p>Birthdate: {selected.birthdate}</p>
-        </div>
-    )
+    if(farmers.selected !== null){
+        let selected = farmers.list.find(f => f._id === farmers.selected)
+        return (
+            <div>
+                <p>Name: {selected.name}</p>
+                <p>Surname: {selected.surname}</p>
+                <p>Mail: {selected.mail}</p>
+                <p>Birthdate: {selected.birthdate}</p>
+            </div>
+        )
+    } else {
+        <p></p>
+    }
 }
