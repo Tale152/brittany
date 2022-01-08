b3ff69bf26f5bddd1d02b43bf2f5e3592c874524
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-01-08 12:52:03 +0100

    Added all ValueReturnedResult test

--- edge/test/brittany/operation-handler/util/ValueReturnedResultTest.h
+++ edge/test/brittany/operation-handler/util/ValueReturnedResultTest.h
@@ -1,6 +1,10 @@
 #include <unity.h>
 #include "operation-handler/interfaces/util/ValueReturnedResult.h"
 
+#define VALUE_RETURNED_RESULT_CODE 100
+#define VALUE_RETURNED_RESULT_VALUE 13
+#define VALUE_RETURNED_RESULT_MESSAGE "OK."
+
 void test_value_returned_result_code(int expected, int actual) {
     TEST_ASSERT_EQUAL(expected, actual);
 }
@@ -24,24 +28,53 @@ void test_value_returned_result_message(std::string expected, std::string actual
 } 
 
 void test_value_returned_code_constructor() {
-    ValueReturnedResult<int> res = ValueReturnedResult<int>(200);
-    test_value_returned_result_code(200, res.code());
+    ValueReturnedResult<int> res = ValueReturnedResult<int>(VALUE_RETURNED_RESULT_CODE);
+    test_value_returned_result_code(VALUE_RETURNED_RESULT_CODE, res.code());
     TEST_ASSERT_FALSE(is_value_returned_result_value_present(res.value()));
     TEST_ASSERT_FALSE(is_value_returned_result_message_present(res.message()));
 }
 
 
-void test_value_returned_full_constructor() {
-    ValueReturnedResult<int> res = ValueReturnedResult<int>(100, std::optional<int>(13), std::optional<std::string>("Ok."));
-    test_value_returned_result_code(100, res.code());
+void test_value_returned_result_is_correct(ValueReturnedResult<int> res) {
+    test_value_returned_result_code(VALUE_RETURNED_RESULT_CODE, res.code());
     TEST_ASSERT_TRUE(is_value_returned_result_value_present(res.value()));
     TEST_ASSERT_TRUE(is_value_returned_result_message_present(res.message()));
-     test_value_returned_result_value(13, res.value().value());
-    test_value_returned_result_message("Ok.", res.message().value());
-    
+    test_value_returned_result_value(VALUE_RETURNED_RESULT_VALUE, res.value().value());
+    test_value_returned_result_message(VALUE_RETURNED_RESULT_MESSAGE, res.message().value()); 
+}
+
+void test_value_returned_full_constructor() {
+    ValueReturnedResult<int> res = ValueReturnedResult<int>(
+        VALUE_RETURNED_RESULT_CODE,
+        std::optional<int>(VALUE_RETURNED_RESULT_VALUE),
+        std::optional<std::string>(VALUE_RETURNED_RESULT_MESSAGE)
+    );
+    test_value_returned_result_is_correct(res);
+}
+
+void test_value_returned_non_opt_constructor() {
+    ValueReturnedResult<int> res = ValueReturnedResult<int>(
+        100,
+        VALUE_RETURNED_RESULT_VALUE,
+        VALUE_RETURNED_RESULT_MESSAGE
+    );
+    test_value_returned_result_is_correct(res);
+}
+
+void test_value_returned_no_message_constructor() {
+    ValueReturnedResult<int> res = ValueReturnedResult<int>(
+        100,
+        VALUE_RETURNED_RESULT_VALUE
+    );
+    test_value_returned_result_code(VALUE_RETURNED_RESULT_CODE, res.code());
+    TEST_ASSERT_TRUE(is_value_returned_result_value_present(res.value()));
+    test_value_returned_result_value(VALUE_RETURNED_RESULT_VALUE, res.value().value());
+    TEST_ASSERT_FALSE(is_value_returned_result_message_present(res.message()));
 }
 
 void test_ValueReturnedResultTest(){
     RUN_TEST(test_value_returned_code_constructor);
     RUN_TEST(test_value_returned_full_constructor);
+    RUN_TEST(test_value_returned_non_opt_constructor);
+    RUN_TEST(test_value_returned_no_message_constructor);
 }
