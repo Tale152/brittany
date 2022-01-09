a2b9a1edbd2237135893698b71ae6a7765f1e2fe
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-01-09 11:51:42 +0100

    Commented ValueReturnedResultFactory

--- edge/lib/brittany/src/operation-handler/interfaces/util/ValueReturnedResultFactory.h
+++ edge/lib/brittany/src/operation-handler/interfaces/util/ValueReturnedResultFactory.h
@@ -7,14 +7,30 @@
 
 template <typename T>
 
+/**
+ * @brief Factory class to create specifics ValueReturnedResult.
+ * 
+ */
 class ValueReturnedResultFactory {
 
 public:
 
+    /**
+     * @brief Return a ValueReturnedResult with a value and without a message with OK as code.
+     * 
+     * @param value the value to insert in the ValueReturnedResult.
+     * @return ValueReturnedResult<T> a new ValueReturnedResult.
+     */
     static ValueReturnedResult<T> OkWithValueResult(T value) {
         return ValueReturnedResult<T>(HttpStatus::OK, value);
     }
 
+    /**
+     * @brief Return a ValueReturnedResult without value, with a message specifiyng that no
+     * resource was found and with NotFound as code.
+     * 
+     * @return ValueReturnedResult<T> a new ValueReturnedResult.
+     */
     static ValueReturnedResult<T> NotFoundResult() {
         return ValueReturnedResult<T>(
                 HttpStatus::NotFound,
@@ -23,6 +39,12 @@ public:
         );
     }
 
+    /**
+     * @brief Return a ValueReturnedResult without value, with a message specifiyng that a bad
+     * request was sent and with BadRequest as code.
+     * 
+     * @return ValueReturnedResult<T>  a new ValueReturnedResult.
+     */
     static ValueReturnedResult<T> BadRequestResult() {
         return ValueReturnedResult<T>(
                 HttpStatus::BadRequest,
