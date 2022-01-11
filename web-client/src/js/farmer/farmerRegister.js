20d916f6ed68863583f7792ff2c2fe5704b6dc47
Tale152 <alessandro.talmi@studio.unibo.it>
2022-01-11 23:58:49 +0000

    handled errors in greenhouse create

--- web-client/src/js/farmer/farmerRegister.js
+++ web-client/src/js/farmer/farmerRegister.js
@@ -3,24 +3,44 @@ import $ from 'jquery'
 import { setIsLoading } from '../../redux/util/actions'
 import farmerList from './farmerList'
 import { authService } from '../../conf'
+import { isNonEmptyString } from '../util/validator'
 
-export default function farmerRegister(token, name, surname, mail, birthdate, password, passwordConfirm, dispatch){
-    dispatch(setIsLoading(true))
-    $.ajaxSetup({
-        contentType: "application/json; charset=utf-8",
-        headers:{
-           'token': token
-        }
-    })
-    //TODO check password
-    $.post(authService + "/farmer/register", createParams(name, surname, mail, birthdate, password))
-        .done(function (result) {
-            farmerList(token, dispatch)
+export default function farmerRegister(token, name, surname, mail, birthdate, password, passwordConfirm, dispatch, alert){
+    if(!validParams(name, surname, mail, birthdate, password, passwordConfirm)){
+        alert.error("Empty fields")
+    } else if(password.trim() !== passwordConfirm.trim()){
+        alert.error("Wrong password confirm")
+    } else {
+        dispatch(setIsLoading(true))
+        $.ajaxSetup({
+            contentType: "application/json; charset=utf-8",
+            headers:{
+            'token': token
+            }
         })
-        .fail(function (result) {
-            console.log("fail")
-            dispatch(setIsLoading(false))
-        })
+        $.post(authService + "/farmer/register", createParams(name, surname, mail, birthdate, password))
+            .done(function () {
+                farmerList(token, dispatch)
+            })
+            .fail(function (result) {
+                if(result.status === 406){
+                    alert.error(result.responseJSON.err)
+                } else {
+                    alert.error("Error contacting server")
+                }
+                dispatch(setIsLoading(false))
+            })
+    }
+}
+
+
+function validParams(name, surname, mail, birthdate, password, passwordConfirm){
+    return isNonEmptyString(name) &&
+        isNonEmptyString(surname) &&
+        isNonEmptyString(mail) &&
+        birthdate !== null &&
+        isNonEmptyString(password) &&
+        isNonEmptyString(passwordConfirm)
 }
 
 function createParams(name, surname, mail, birthdate, password) {
