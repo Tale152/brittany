2e43a17a1393e656d26a949f7a72a381cef6d314
Tale152 <alessandro.talmi@studio.unibo.it>
2022-01-11 23:37:37 +0000

    handled empty name in greenhouse creation

--- web-client/src/js/greenhouse/greenhouseCreate.js
+++ web-client/src/js/greenhouse/greenhouseCreate.js
@@ -3,23 +3,36 @@ import $ from 'jquery'
 import { setIsLoading } from '../../redux/util/actions'
 import greenhouseList from './greenhouseList'
 import { authService } from '../../conf'
+import { isNonEmptyString } from '../util/validator'
 
-export default function greenhouseCreate(token, name, dispatch){
-    dispatch(setIsLoading(true))
-    $.ajaxSetup({
-        contentType: "application/json; charset=utf-8",
-        headers:{
-           'token': token
-        }
-    })
-    $.post(authService + "/greenhouse/create", createParams(name))
-        .done(function (result) {
-            greenhouseList(token, dispatch)
+export default function greenhouseCreate(token, name, dispatch, alert){
+    if(validParams(name)){
+        dispatch(setIsLoading(true))
+        $.ajaxSetup({
+            contentType: "application/json; charset=utf-8",
+            headers:{
+               'token': token
+            }
         })
-        .fail(function (result) {
-            console.log("fail")
-            dispatch(setIsLoading(false))
-        })
+        $.post(authService + "/greenhouse/create", createParams(name))
+            .done(function (result) {
+                greenhouseList(token, dispatch)
+            })
+            .fail(function (result) {
+                if(result.status === 406){
+                    alert.error(result.responseJSON.err)
+                } else {
+                    alert.error("Error contacting server")
+                }
+                dispatch(setIsLoading(false))
+            })
+    } else {
+        alert.error("Name cannot be empty")
+    }
+}
+
+function validParams(name){
+    return isNonEmptyString(name)
 }
 
 function createParams(name) {
