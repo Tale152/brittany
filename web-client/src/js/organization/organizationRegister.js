f98f68bb61fe08cde48667a1b1417ed419fdf2f2
Tale152 <alessandro.talmi@studio.unibo.it>
2022-01-11 19:24:34 +0000

    handled errors in organization register

--- web-client/src/js/organization/organizationRegister.js
+++ web-client/src/js/organization/organizationRegister.js
@@ -4,23 +4,44 @@ import { setToken } from '../../redux/identity/actions'
 import { setIsLoading } from '../../redux/util/actions'
 import organizationInfo from './organizationInfo'
 import { authService } from '../../conf'
+import { isNonEmptyString } from '../util/validator'
 
 $.ajaxSetup({
     contentType: "application/json; charset=utf-8"
 })
 
-export default function organizationRegister(organizationName, name, surname, mail, birthdate, password, passwordConfirm, dispatch){
-    dispatch(setIsLoading(true))
-    $.post(authService + "/organization/create", createParams(organizationName, name, surname, mail, birthdate, password))
+export default function organizationRegister(organizationName, name, surname, mail, birthdate, password, passwordConfirm, dispatch, alert){
+    if(!validParams(organizationName, name, surname, mail, birthdate, password, passwordConfirm)){
+        alert.error("Empty fields")
+    } else if(password.trim() !== passwordConfirm.trim()){
+        alert.error("Wrong password confirm")
+    } else {
+        dispatch(setIsLoading(true))
+        $.post(authService + "/organization/create", createParams(organizationName, name, surname, mail, birthdate, password))
         .done(function (result) {
             dispatch(setToken(result.token))
             organizationInfo(result.token, dispatch)
         })
         .fail(function (result) {
-            console.log("fail")
+            if(result.status === 406){
+                alert.error(result.responseJSON.err)
+            } else {
+                alert.error("Error contacting server")
+            }
         }).always(function() {
             dispatch(setIsLoading(false))
         })
+    }
+}
+
+function validParams(organizationName, name, surname, mail, birthdate, password, passwordConfirm){
+    return isNonEmptyString(organizationName) &&
+        isNonEmptyString(name) &&
+        isNonEmptyString(surname) &&
+        isNonEmptyString(mail) &&
+        isNonEmptyString(birthdate) &&
+        isNonEmptyString(password) &&
+        isNonEmptyString(passwordConfirm)
 }
 
 function createParams(organizationName, name, surname, mail, birthdate, password) {
