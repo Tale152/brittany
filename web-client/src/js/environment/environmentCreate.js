09e423cdb1350837b5c39db1e8644bd3d7f9b1c7
Tale152 <alessandro.talmi@studio.unibo.it>
2022-01-12 09:49:23 +0000

    handled errors in environment create

--- web-client/src/js/environment/environmentCreate.js
+++ web-client/src/js/environment/environmentCreate.js
@@ -3,24 +3,40 @@ import $ from 'jquery'
 import { setIsLoading } from '../../redux/util/actions'
 import environmentList from './environmentList'
 import { authService } from '../../conf'
+import { isNonEmptyString } from '../util/validator'
 
-export default function greenhouseCreate(token, greenhouseId, name, password, passwordConfirm, dispatch){
-    //TODO check password
-    dispatch(setIsLoading(true))
-    $.ajaxSetup({
-        contentType: "application/json; charset=utf-8",
-        headers:{
-           'token': token
-        }
-    })
-    $.post(authService + "/environment/create", createParams(name, password, greenhouseId))
-        .done(function (result) {
-            environmentList(token, greenhouseId, dispatch)
+export default function greenhouseCreate(token, greenhouseId, name, password, passwordConfirm, dispatch, alert){
+    if(!validParams(name, password, passwordConfirm)){
+        alert.error("Empty fields")
+    } else if(password.trim() !== passwordConfirm.trim()){
+        alert.error("Wrong password confirm")
+    } else {
+        dispatch(setIsLoading(true))
+        $.ajaxSetup({
+            contentType: "application/json; charset=utf-8",
+            headers:{
+               'token': token
+            }
         })
-        .fail(function (result) {
-            console.log("fail")
-            dispatch(setIsLoading(false))
-        })
+        $.post(authService + "/environment/create", createParams(name, password, greenhouseId))
+            .done(function (result) {
+                environmentList(token, greenhouseId, dispatch)
+            })
+            .fail(function (result) {
+                if(result.status === 406){
+                    alert.error(result.responseJSON.err)
+                } else {
+                    alert.error("Error contacting server")
+                }
+                dispatch(setIsLoading(false))
+            })
+    }
+}
+
+function validParams(name, password, passwordConfirm){
+    return isNonEmptyString(name) &&
+        isNonEmptyString(password) &&
+        isNonEmptyString(passwordConfirm)
 }
 
 function createParams(name, password, greenhouseId) {
--- web-client/src/react/greenhouses/components/CreateEnvironmentButton.js
+++ web-client/src/react/greenhouses/components/CreateEnvironmentButton.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { useAlert } from 'react-alert'
 
 import environmentCreate from '../../../js/environment/environmentCreate'
 import CustomCenteredButton from '../../_common/CustomCenteredButton'
@@ -9,6 +10,7 @@ import FormPassword from '../../_common/form/input/FormPassword'
 
 export default function CreateEnvironmentButton() {
 
+    const alert = useAlert()
     const dispatch = useDispatch()
     const [showModal, setShowModal] = useState(false)
     const [name, setName] = useState("")
@@ -16,6 +18,14 @@ export default function CreateEnvironmentButton() {
     const [passwordConfirm, setPasswordConfirm] = useState("")
     let token = useSelector(state => state.identity.token)
     let greenhouseId = useSelector(state => state.greenhouses.selected)
+
+    function tryCreateEnvironment(){
+        environmentCreate(token, greenhouseId, name, password, passwordConfirm, dispatch, alert)
+        setShowModal(false)
+        setName("")
+        setPassword("")
+        setPasswordConfirm("")
+    }
 
     return (
         <>
@@ -33,34 +43,22 @@ export default function CreateEnvironmentButton() {
                             text = { "Name:" }
                             placeholder = { "Enter new Greenhouse's name" }
                             onChange = { input => setName(input) }
-                            onEnter = { () => {
-                                environmentCreate(token, greenhouseId, name, password, passwordConfirm, dispatch)
-                                setShowModal(false)
-                            }}
+                            onEnter = { tryCreateEnvironment }
                         />
                         <FormPassword
                             text = { "Password:" }
                             placeholder = { "Enter password" }
                             onChange = { input => setPassword(input) }
-                            onEnter = { () => {
-                                environmentCreate(token, greenhouseId, name, password, passwordConfirm, dispatch)
-                                setShowModal(false)
-                            }}
+                            onEnter = { tryCreateEnvironment }
                         />
                         <FormPassword
                             text = { "Confirm password:" }
                             placeholder = { "Enter password again" }
                             onChange = { input => setPasswordConfirm(input) }
-                            onEnter = { () => {
-                                environmentCreate(token, greenhouseId, name, password, passwordConfirm, dispatch)
-                                setShowModal(false)
-                            }}
+                            onEnter = { tryCreateEnvironment }
                         />
                         <CustomCenteredButton
-                            onClick = { () => {
-                                environmentCreate(token, greenhouseId, name, password, passwordConfirm, dispatch)
-                                setShowModal(false)
-                            }}
+                            onClick = { tryCreateEnvironment }
                             text = { "Create" }
                         />
                     </>
