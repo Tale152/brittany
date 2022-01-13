b94897c0d55592b84a4c03033de37c489d61ed8e
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-01-13 12:44:11 +0100

    Added Temperature Mock module to main

--- edge/src/modules/mock-temperature-humidity-sensor.h
+++ edge/src/modules/mock-temperature-humidity-sensor.h
@@ -1,14 +1,17 @@
-#ifdef BRITTANY_MAIN_MOCK_TEMPERATURE_HUMIDITY_SENSOR
+//#ifdef BRITTANY_MAIN_MOCK_TEMPERATURE_HUMIDITY_SENSOR
 
+#include "mock-temperature-sensor/hw/MockTemperatureSensorHw.h"
+#include "mock-temperature-sensor/modules/MockTemperatureModule.h"
 #include "mock-humidity-sensor/hw/MockHumiditySensorHw.h"
 #include "mock-humidity-sensor/modules/MockHumidityModule.h"
 
 Edge* edge() {
     MockHumiditySensorHw* humSensor = new MockHumiditySensorHw("hum");
-    std::list<MockHumiditySensorHw*> sensors = std::list<MockHumiditySensorHw*>({humSensor});
+    MockTemperatureSensorHw* tempSensor = new MockTemperatureSensorHw("temp");
     std::list<Module*> modules;
-    modules.push_back(new MockHumidityModule(sensors));
+    modules.push_back(new MockHumidityModule({humSensor}));
+    modules.push_back(new MockTemperatureModule({tempSensor}));
     return new Edge("Mock Humidity and Temperature Edge", modules);
 }
 
-#endif
+//#endif
