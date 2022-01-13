b51d926957756c08e975d585af2c09b1f986f04b
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-01-13 18:05:53 +0100

    MockIsOnHandler now uses a better class as superclass

--- edge/lib/brittany-mock/src/mock-digital-light/operation-handler/MockIsOnDigitalLightHandler.h
+++ edge/lib/brittany-mock/src/mock-digital-light/operation-handler/MockIsOnDigitalLightHandler.h
@@ -4,14 +4,15 @@
 #include <list>
 #include <string>
 #include <json/json.h>
-#include "operation-handler/interfaces/ValueReturnedAfterActionHandlerInterface.h"
+#include "operation-handler/interfaces/RetrieveValueFromComponentInterface.h"
 #include "mock-digital-light/hw/MockDigitalLightHw.h"
 #include "operation-handler/OperationHandlerResult.h"
 #include "HttpStatusCodes_C++.h"
 #include "util.h"
 #include <optional>
 
-class MockIsOnDigitalLightHandler : public ValueReturnedAfterActionHandlerInterface<bool> {
+class MockIsOnDigitalLightHandler
+: public RetrieveValueFromComponentInterface<MockDigitalLightHw, bool> {
 
 public:
 
@@ -19,26 +20,21 @@ public:
         std::string name,
         std::string path,
         std::list<MockDigitalLightHw*> components
-    ): ValueReturnedAfterActionHandlerInterface<bool>(
+    ): RetrieveValueFromComponentInterface<MockDigitalLightHw, bool>(
         name,
         path,
         OperationType::PROPERTY,
-        Type::BOOLEAN
+        Type::BOOLEAN,
+        components
     ) {
-        _components = components;
+
     };
 
 private:
 
-    std::optional<bool> retrieveValue(Json::Value args) {
-        std::optional<MockDigitalLightHw*> oc = find_by_id(_components, args["id"].asCString());
-        if(oc.has_value()){
-            return std::optional(oc.value() -> isOn());
-        }
-        return std::nullopt;
+    std::optional<bool> sub_operation(MockDigitalLightHw* hw, Json::Value args) {
+        return std::optional(hw -> isOn());
     }
-    
-    std::list<MockDigitalLightHw*> _components;
 
 };
 
