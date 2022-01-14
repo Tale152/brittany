cd1ce003ce8b7706c89ad1fe77c886484b07c44c
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-01-14 17:36:12 +0100

    Moved modules in main in two different folders

--- edge/src/main.cpp
+++ edge/src/main.cpp
@@ -4,10 +4,10 @@
 #include "web-server/Esp8266WebServer.h"
 #include "wifi_secret.h"
 #include "modules/Module.h"
-#include "modules/dht22.h"
-#include "modules/mock-digital-light.h"
-#include "modules/mock-temperature-humidity-sensor.h"
-#include "modules/mock-temperature-humidity-actuator.h"
+#include "modules/concrete/concrete-dht22.h"
+#include "modules/mock/mock-digital-light.h"
+#include "modules/mock/mock-temperature-humidity-sensor.h"
+#include "modules/mock/mock-temperature-humidity-actuator.h"
 
 Esp8266WebServer* server;
 
