fd61b47aeb63f86427c9f18ec03bd9484848d72b
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-01-14 14:53:52 +0100

    Added dehumidifer module

--- edge/lib/brittany-mock/src/mock-humidity-actuator/modules/MockDehumidifierModule.h
+++ edge/lib/brittany-mock/src/mock-humidity-actuator/modules/MockDehumidifierModule.h
@@ -1,5 +1,5 @@
-#ifndef BRITTANY_MOCK_HUMIDIFIER_MODULE_H
-#define BRITTANY_MOCK_HUMIDIFIER_MODULE_H
+#ifndef BRITTANY_MOCK_DEHUMIDIFIER_MODULE_H
+#define BRITTANY_MOCK_DEHUMIDIFIER_MODULE_H
 
 #include <list>
 #include "operation-handler/interfaces/components/TurnOffSwitchableHandler.h"
@@ -9,30 +9,30 @@
 #include "modules/ComponentModule.h"
 #include "modules/ModuleNames.h"
 
-class MockHumidifierModule : public ComponentModule<MockSwitchableHw> {
+class MockDehumidifierModule : public ComponentModule<MockSwitchableHw> {
 
 public:
 
-    MockHumidifierModule(std::list<MockSwitchableHw*> components)
+    MockDehumidifierModule(std::list<MockSwitchableHw*> components)
     : ComponentModule<MockSwitchableHw>(module_as_string(ModuleNames::Humidity), components) {
         _handlers.push_back(
             new TurnOnSwitchableHandler<MockSwitchableHw>(
-               "humidifyOn",
-               as_route("humidifyOn"),
+               "dehumidifyOn",
+               as_route("dehumidifyOn"),
                components
             )
         );
         _handlers.push_back(
             new TurnOffSwitchableHandler<MockSwitchableHw>(
-               "humidifyOff",
-               as_route("humidifyOff"),
+               "dehumidifyOff",
+               as_route("dehumidifyOff"),
                components
             )
         );
         _handlers.push_back(
             new IsOnSwitchableHandler<MockSwitchableHw>(
-               "isHumidifyOn",
-               as_route("isHumidifyOn"),
+               "isDehumidifyOn",
+               as_route("isDehumidifyOn"),
                components
             )
         );
@@ -40,4 +40,4 @@ public:
 
 };
 
-#endif //BRITTANY_MOCK_HUMIDIFIER_MODULE_H
+#endif //BRITTANY_MOCK_DEHUMIDIFIER_MODULE_H
--- edge/src/modules/mock-temperature-humidity-actuator.h
+++ edge/src/modules/mock-temperature-humidity-actuator.h
@@ -5,16 +5,20 @@
 #include "mock-temperature-actuator/modules/MockTemperatureCoolerModule.h"
 #include "mock-temperature-actuator/modules/MockTemperatureHeaterModule.h"
 #include "mock-humidity-actuator/modules/MockHumidifierModule.h"
+#include "mock-humidity-actuator/modules/MockDehumidifierModule.h"
 
 Edge* edge() {
     MockSwitchableHw* heater = new MockSwitchableHw("heater");
     MockSwitchableHw* cooler = new MockSwitchableHw("cooler");
     MockSwitchableHw* humidifier = new MockSwitchableHw("humidifier");
+    MockSwitchableHw* dehumidifier = new MockSwitchableHw("dehumidifier");
     std::list<Module*> modules;
     modules.push_back(new MockTemperatureHeaterModule({heater}));
     modules.push_back(new MockTemperatureCoolerModule({cooler}));
     modules.push_back(new MockHumidifierModule({humidifier}));
-    return new Edge("Mock Temperature Edge that can increase and lower the temperature.", modules);
+    modules.push_back(new MockDehumidifierModule({dehumidifier}));
+    std::string edge_desc = "Mock Temperature Edge that can increase and lower the temperature and the humidity.";
+    return new Edge(edge_desc, modules);
 }
 
 #endif
