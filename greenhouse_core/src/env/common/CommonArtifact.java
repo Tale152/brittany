d8a1c9ba77cf1c1a94f102ff4637558e03d86c08
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-04-28 13:08:07 +0000

    Added JavaDoc to CommonArtifact

--- greenhouse_core/src/env/common/CommonArtifact.java
+++ greenhouse_core/src/env/common/CommonArtifact.java
@@ -13,9 +13,12 @@ import utility.setting.Settings;
 
 /**
  * CommonArtifact is the core Artifact of this JaCaMo application.
- * Its main role is to wait for the retrieval of the authentication token, and
- * then it starts the
- * sampling activity, initializing all the data that the other agents will need.
+ * It is used as a common container of the information that mostly all the 
+ * agents need.
+ * In order to avoid an high dependency between agents, to retrieve the basic
+ * information it is used this Artifact.
+ * It shares the settings, the tds and the componets using observable properties,
+ * and the agents that need those information have plans to store them in their BB.
  *
  */
 public class CommonArtifact extends Artifact {
@@ -29,11 +32,18 @@ public class CommonArtifact extends Artifact {
 		this.settings = Optional.empty();
 		this.thingDescriptors = new ArrayList<>();
 
+		//defines the observable property during the initialization
 		defineObsProperty("setupSettings", this.settings);
 		defineObsProperty("setupComponents", this.components);
 		defineObsProperty("setupTds", this.thingDescriptors);
 	}
 
+	/**
+	 * Operation used to share a list of all the Components that are found in the greenhouse
+	 * to all the agents that observe this Artifact and have "setupComponents" in their plans.
+	 * 
+	 * @param components the list of Components found.
+	 */
 	@OPERATION
 	void shareComponents(final List<Component> components) {
 		if (!this.components.equals(components)) {
@@ -42,6 +52,13 @@ public class CommonArtifact extends Artifact {
 		updateObsProperty("setupComponents", this.components);
 	}
 
+	/**
+	 * Operation used to share the Settings that are found in the greenhouse
+	 * to all the agents that observe this Artifact and have "setupSettings" in their plans.
+	 * The settings are Optional, because their might be not set by the user.
+	 * 
+	 * @param settings an optional of the Settings found.
+	 */
 	@OPERATION
 	void shareSettings(final Optional<Settings> settings) {
 		if (!this.settings.equals(settings)) {
@@ -50,6 +67,12 @@ public class CommonArtifact extends Artifact {
 		updateObsProperty("setupSettings", this.settings);
 	}
 
+	/**
+	 * Operation used to share a list of all the Thing Descriptors that are found
+	 * to all the agents that observe this Artifact and have "setupTds" in their plans.
+	 * 
+	 * @param thingDescriptors the list of Thing Descriptors found.
+	 */
 	@OPERATION
 	void shareThingDescriptors(final List<ConsumedThing> thingDescriptors) {
 		if (!this.thingDescriptors.equals(thingDescriptors)) {
