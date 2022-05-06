ee7c0141cad95c1b39cafa08358f90d51304e24d
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-05-06 12:15:46 +0000

    Added JavaDoc and refactored PersistenceArtifact

--- greenhouse_core/src/env/communication/PersistenceArtifact.java
+++ greenhouse_core/src/env/communication/PersistenceArtifact.java
@@ -14,19 +14,58 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import utility.Sample;
 
+/**
+ * PersistenceArtifact is an Artifact used to upload samples to an external
+ * service.
+ * When a sample is out of a specific delta, 
+ * then the sample is going to be uploaded.
+ */
 public class PersistenceArtifact extends Artifact {
 
 	private final static String PERSISTENCE_SERVICE_URL = "https://brittany-persistence-service.herokuapp.com:443/";
+
 	private OkHttpClient client;
 
 	void init() {
 		this.client = new OkHttpClient();
 	}
 
+	/**
+	 * Operation used to send an HTTP request to the service in order to upload the
+	 * sample retrieved.
+	 * 
+	 * @param sample the sample that has to be uploaded because it is out of the
+	 *               delta specified.
+	 * @param token  the token used to correctly send the HTTP request to the
+	 *               service.
+	 */
 	@OPERATION
 	void uploadPersistence(final Sample sample, final String token) {
-		HttpUrl.Builder urlBuilder = HttpUrl.parse(PERSISTENCE_SERVICE_URL + sample.getCategory() + "/register")
-				.newBuilder();
+		try (Response response = client.newCall(getUploadRequest(sample, token)).execute()) {
+			if (!response.isSuccessful()) {
+				throw new IOException("Unexpected code " + response);
+			} else {
+				System.out.println(sample + " correcty uploaded");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Utility method used to build the HTTP request that is going to be send to the
+	 * service.
+	 * It contains tha value and the timestamp of the sample in the body and the
+	 * token in the head of the request.
+	 * 
+	 * @param sample the sample that has been registered to be out of a specified
+	 *               delta and that need to be uploaded.
+	 * @param token  the token used to correctly send the HTTP request to the
+	 *               service.
+	 * @return the HTTP request to send to the persistence service.
+	 */
+	private Request getUploadRequest(final Sample sample, final String token) {
+		HttpUrl.Builder urlBuilder = HttpUrl.parse(PERSISTENCE_SERVICE_URL + sample.getCategory() + "/register").newBuilder();
 		String url = urlBuilder.build().toString();
 
 		JsonObject jsonSample = new JsonObject();
@@ -36,21 +75,7 @@ public class PersistenceArtifact extends Artifact {
 		RequestBody body = RequestBody.create(jsonSample.toString(),
 				MediaType.parse("application/json; charset=utf-8"));
 
-		Request request = new Request.Builder()
-				.addHeader("token", token)
-				.url(url)
-				.post(body)
-				.build();
-
-		try (Response response = client.newCall(request).execute()) {
-			if (!response.isSuccessful()) {
-				throw new IOException("Unexpected code " + response);
-			} else {
-				System.out.println(sample + " correcty uploaded");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		return new Request.Builder().addHeader("token", token).url(url).post(body).build();
 	}
 
 }
