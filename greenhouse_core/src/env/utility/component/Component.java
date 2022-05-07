b0c4e55fa6fe6d64945142c04505545d6a0967d3
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-05-07 18:18:20 +0000

    Added JavaDoc to Component

--- greenhouse_core/src/env/utility/component/Component.java
+++ greenhouse_core/src/env/utility/component/Component.java
@@ -9,6 +9,7 @@ import java.util.Optional;
  * action names that have been retrieved from the thing descriptor.
  */
 public class Component {
+
 	private final String edgeIp;
 	private final String id;
 	private final String category;
@@ -16,14 +17,14 @@ public class Component {
 	private final List<String> actions;
 
 	/**
-	 * Component constructor, used to give all the information that 
+	 * Component constructor, used to give all the information that
 	 * a Component needs.
 	 * 
-	 * @param edgeIp 	 the ip of the edge where the component is connected.
-	 * @param id 		 the id of the component.
+	 * @param edgeIp     the ip of the edge where the component is connected.
+	 * @param id         the id of the component.
 	 * @param category   the category of the component.
 	 * @param properties the list of the properties' names that the component have.
-	 * @param actions 	 the list of the actions' names tha the component can perform.
+	 * @param actions    the list of the actions' names tha the component can perform.
 	 */
 	protected Component(final String edgeIp, final String id, final String category,
 			final List<String> properties, final List<String> actions) {
@@ -34,41 +35,79 @@ public class Component {
 		this.actions = actions;
 	}
 
+	/**
+	 * A component is always connected to an edge, which has an ip.
+	 * This method returns the edge's ip where the component is connected to.
+	 * 
+	 * @return the edge's ip that is resposible of the component.
+	 */
 	public String getEdgeIp() {
 		return edgeIp;
 	}
 
+	/**
+	 * A component has an identificator that is unique for the edge 
+	 * it is connected to.
+	 * 
+	 * @return the unique id of the component.
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * A component has a category it falls into, which defines the kind
+	 * of sensor of actuator it is.
+	 * 
+	 * @return the category of the component.
+	 */
 	public String getCategory() {
 		return category;
 	}
 
+	/**
+	 * A component has a list of names of the properties it can access,
+	 * which are extracted from the thing descriptor of the edge it
+	 * is connected to.
+	 * 
+	 * @return the list of names of the properties that the component can
+	 * access.
+	 */
 	public List<String> getProperties() {
 		return properties;
 	}
 
 	/**
+	 * Method used to extract a property using a known 
+	 * part of the name of the property wanted.
 	 * 
-	 * 
-	 * @param subString
-	 * @return
+	 * @param subString the part of the property name known.
+	 * @return the complete name of the property found 
+	 * starting from the substring provided.
 	 */
 	public Optional<String> getPropertyBySubString(final String subString) {
 		return this.properties.stream().filter(s -> s.contains(subString)).findFirst();
 	}
 
+	/**
+	 * A component has a list of names of the action it can perform,
+	 * which are extracted from the thing descriptor of the edge it
+	 * is connected to.
+	 * 
+	 * @return the list of names of the actions that the component can
+	 * perform.
+	 */
 	public List<String> getActions() {
 		return actions;
 	}
 
 	/**
+	 * Method used to extract an action using a known 
+	 * part of the name of the action wanted.
 	 * 
-	 * 
-	 * @param subString
-	 * @return
+	 * @param subString the part of the action name known.
+	 * @return the complete name of the action found 
+	 * starting from the substring provided.
 	 */
 	public Optional<String> getActionBySubString(final String subString) {
 		return this.actions.stream().filter(s -> s.contains(subString)).findFirst();
