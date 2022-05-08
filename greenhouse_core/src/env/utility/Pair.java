b36f159607cde2a344333c42d99d08976aaf492d
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-05-08 11:26:38 +0000

    Added JavaDoc to Pair

--- greenhouse_core/src/env/utility/Pair.java
+++ greenhouse_core/src/env/utility/Pair.java
@@ -1,19 +1,39 @@
 package utility;
 
+/**
+ * Pair is an generic utility class used to contain in
+ * a single object two values logically connected in some way.
+ */
 public class Pair<X,Y> {
 	
 	private final X x;
 	private final Y y;	
 	
+	/**
+	 * Constructor of Pair.
+	 * 
+	 * @param x the first generic value of the Pair.
+	 * @param y the second generic value of the Pair.
+	 */
 	public Pair(final X x, final Y y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * A Pair has a generic first element.
+	 * 
+	 * @return the first generic value of the Pair.
+	 */
 	public X getX() {
 		return x;
 	}
 
+	/**
+	 * A Pair has a generic second element.
+	 * 
+	 * @return the second generic value of the Pair.
+	 */
 	public Y getY() {
 		return y;
 	}
