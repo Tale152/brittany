52162c3dbde8f080750086902661cb01d90a861c
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-05-08 12:20:55 +0000

    Added JavaDoc to ThingDescriptorUtility

--- greenhouse_core/src/env/utility/ThingDescriptorUtility.java
+++ greenhouse_core/src/env/utility/ThingDescriptorUtility.java
@@ -21,23 +21,28 @@ import utility.component.Component;
 public class ThingDescriptorUtility {
 
 	/**
+	 * Utility method used to find in a list of thing descriptors a specific
+	 * thing descriptor by its identifier.
 	 * 
-	 * 
-	 * @param thingDescriptors
-	 * @param id
-	 * @return
+	 * @param thingDescriptors the list of thing descriptors.
+	 * @param id 			   the id of the thing descriptor wanted.
+	 * @return an optional of the thing descriptor with the specified id if found,
+	 * and optional of empty otherwise.
 	 */
 	public static Optional<ConsumedThing> getThingDescriptor(final List<ConsumedThing> thingDescriptors, final String id) {
 		return thingDescriptors.stream().filter(t -> t.getId().equals(id)).findFirst();
 	}
 
 	/**
+	 * Utility method used to get a list of actions and/or properties names from
+	 * the thing descriptor of a specific component, of a specific category.
 	 * 
-	 * 
-	 * @param td
-	 * @param category
-	 * @param componentId
-	 * @return
+	 * @param td 		  the thing descriptor that contains the actions or the properties.
+	 * @param category	  the category of the action or properties wanted.
+	 * @param componentId the id of the component of which the names of properties or
+	 * 					  actions are wanted.
+	 * @return a list of names of the action or the properties of a specific component, based
+	 * on its category.
 	 */
 	public static List<String> getNamesByCategory(final JsonObject td, final String category, final String componentId) {
 		List<String> names = new ArrayList<>();
